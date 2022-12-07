c92a653c11ee35c22e7224d7870f26b53d075aa0
lakitakharvante <kharvantelakita@gmail.com>
2022-12-07 22:48:59 +0530

    addting config1 file

---
+++ config1.py
@@ -0,0 +1,8 @@
+import os 
+
+CSV_FILE_PATH = os.path.join(os.getcwd(),r'CSV_Files\medical_insurance.csv')
+
+ARTIFACTS_FOLDER_PATH = 'artifacts'
+
+model_file_path = os.path.join(ARTIFACTS_FOLDER_PATH,)
+PORT_NUMBER = 5004
